bf3bc0f613b5165f1b91245e1dca31e598bb227c
yaojenkuo <tonykuoyj@gmail.com>
2017-08-08 12:29:45 +0800

    added validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,11 @@
 class Course < ApplicationRecord
     belongs_to :category
     belongs_to :teacher
+    validates_associated :category
+    validates :category, presence: true
 
+    validates_presence_of :title, :description, :length, :startdate, :register_url
+    validates :course_img, attachment_presence: true
     has_attached_file :course_img, styles: { course_index: "250x350>", course_show: "325x475>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :course_img, content_type: /\Aimage\/.*\z/
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,8 @@
 class Teacher < ApplicationRecord
     has_many :courses
+    validates_presence_of :name, :experience
 
     has_attached_file :teacher_img, styles: { teacher_index: "250x350>", teacher_show: "325x475>" }, default_url: "/images/:style/missing.png"
+    validates :teacher_img, attachment_presence: true
     validates_attachment_content_type :teacher_img, content_type: /\Aimage\/.*\z/
 end
